14bc0c376b036ce7a7983cd3c61c2c4c2a40068a
Peter Munch <peterrmuench@gmail.com>
2024-02-25 22:31:50 +0100

    Work on parameters

--- main.cc
+++ main.cc
@@ -127,7 +127,21 @@ public:
   initialize() override
   {
     const auto &matrix = op.get_system_matrix();
-    precon.initialize(matrix);
+
+    typename TrilinosWrappers::PreconditionAMG::AdditionalData ad;
+
+    ad.elliptic              = false;
+    ad.higher_order_elements = false;
+    ad.n_cycles              = 1;
+    ad.aggregation_threshold = 1e-14;
+    ad.constant_modes        = {}; // TODO
+    ad.smoother_sweeps       = 2;
+    ad.smoother_overlap      = 1;
+    ad.output_details        = false;
+    ad.smoother_type         = "ILU";
+    ad.coarse_type           = "ILU";
+
+    precon.initialize(matrix, ad);
   }
 
   void
