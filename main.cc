11ace2e8c5cbb24e4ae84f37da7d971b992ab8f4
Peter Munch <peterrmuench@gmail.com>
2024-03-01 10:59:46 +0100

    Rename variables

--- main.cc
+++ main.cc
@@ -1198,7 +1198,6 @@ private:
         const auto delta_1 = this->delta_1[cell];
         const auto delta_2 = this->delta_2[cell];
         const auto weight  = this->time_integrator_data.get_primary_weight();
-        const auto theta   = this->theta;
         const auto nu      = this->nu;
 
         integrator.evaluate(EvaluationFlags::EvaluationFlags::values |
@@ -1215,38 +1214,25 @@ private:
             const VectorizedArray<Number>                 p_value = value[dim];
             const Tensor<1, dim, VectorizedArray<Number>> p_gradient =
               gradient[dim];
-            Tensor<1, dim, VectorizedArray<Number>> p_bar_gradient =
-              theta * gradient[dim];
 
             const Tensor<1, dim, VectorizedArray<Number>> u_star_value =
               star_value[cell][q];
             Tensor<1, dim, VectorizedArray<Number>> u_time_derivative;
-            Tensor<2, dim, VectorizedArray<Number>> u_bar_gradient;
+            Tensor<2, dim, VectorizedArray<Number>> u_gradient;
 
             for (unsigned int d = 0; d < dim; ++d)
               {
                 u_time_derivative[d] = value[d] * weight;
-                u_bar_gradient[d]    = theta * gradient[d];
-              }
-
-            if (evaluate_residual)
-              u_time_derivative += u_time_derivative_old[cell][q];
-
-            if (evaluate_residual && (theta[0] != 1.0))
-              {
-                u_bar_gradient += (VectorizedArray<Number>(1.0) - theta) *
-                                  old_gradient[cell][q];
-                p_bar_gradient += (VectorizedArray<Number>(1.0) - theta) *
-                                  old_gradient_p[cell][q];
+                u_gradient[d]        = gradient[d];
               }
 
             // precompute: div(B)
-            VectorizedArray<Number> div_bar = u_bar_gradient[0][0];
+            VectorizedArray<Number> div_bar = u_gradient[0][0];
             for (unsigned int d = 1; d < dim; ++d)
-              div_bar += u_bar_gradient[d][d];
+              div_bar += u_gradient[d][d];
 
             // precompute: S⋅∇B
-            const auto s_grad_b = u_bar_gradient * u_star_value;
+            const auto s_grad_b = u_gradient * u_star_value;
 
             // velocity block:
             //  a)  (v, ∂t(u))
@@ -1263,13 +1249,13 @@ private:
 
             //  d)  (ε(v), νε(B))
             for (unsigned int d = 0; d < dim; ++d)
-              gradient_result[d][d] += u_bar_gradient[d][d] * nu;
+              gradient_result[d][d] += u_gradient[d][d] * nu;
 
             for (unsigned int e = 0, counter = dim; e < dim; ++e)
               for (unsigned int d = e + 1; d < dim; ++d, ++counter)
                 {
                   const auto tmp =
-                    (u_bar_gradient[d][e] + u_bar_gradient[e][d]) * (nu * 0.5);
+                    (u_gradient[d][e] + u_gradient[e][d]) * (nu * 0.5);
                   gradient_result[d][e] += tmp;
                   gradient_result[e][d] += tmp;
                 }
@@ -1279,7 +1265,7 @@ private:
               (delta_1) * ((consider_time_deriverative ?
                               u_time_derivative :
                               Tensor<1, dim, VectorizedArray<Number>>()) +
-                           p_bar_gradient + s_grad_b);
+                           p_gradient + s_grad_b);
             for (unsigned int d0 = 0; d0 < dim; ++d0)
               for (unsigned int d1 = 0; d1 < dim; ++d1)
                 gradient_result[d0][d1] += u_star_value[d1] * residual[d0];
