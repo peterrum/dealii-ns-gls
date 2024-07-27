502db70a9665bbba9c1d1457f56db45053f145e2
Peter Munch <peterrmuench@gmail.com>
2024-07-27 22:00:45 +0200

    Work on input files

--- input/input_hoffmann_2D_ReInf_2D.sh
+++ input/input_hoffmann_2D_ReInf_2D.sh
@@ -1,7 +1,10 @@
+foldername=roughness_2
 
-for filename in roughness_2/*.json; do
+for full_name in $foldername/*.json; do
+    filename=$(basename ${full_name})
     echo $filename
 
-    mpirun -np 24 ./gls-app $filename | tee ${filename::-4}"txt"
-    cp $(ls -t *.vtu | head -1) ${filename::-4}"vtu"
+    mpirun -np 24 ./gls-app $foldername/$filename | tee $foldername/${filename::-4}"txt"
+    cp $(ls -t results_hoffmann_2D_ReInf.*.*.vtu | head -1) $foldername/${filename::-4}"vtu"
+    cp $(ls -t results_hoffmann_2D_ReInf.*_drag_lift_pressure.m | head -1) $foldername/${filename::-4}"m"
 done
